1d0c3893282bed83113fe045a408e65caa1e6455
jaz <jazumo00@gmail.com>
2022-09-12 13:14:06 -0600

    Tutorial AsyncStorage

--- app/screens/AlmacenarScreen.js
+++ app/screens/AlmacenarScreen.js
@@ -1,11 +1,12 @@
 import React, {Component, createContext} from 'react'
-import { useState, useContext} from 'react';
-import {View, Text, StyleSheet, Button, Alert} from "react-native";
+import { useState, useContext, useEffect} from 'react';
+import {View, Text, StyleSheet, Button, Alert, TextInput} from "react-native";
 import * as DocumentPicker from 'expo-document-picker';
 import ProveedorArchivos from '../context/ProveedorArchivos';
 import { ArchivosContext } from '../context/ProveedorArchivos';
 import { documentDirectory, EncodingType, writeAsStringAsync } from 'expo-file-system';
 import * as MediaLibrary from 'expo-media-library';
+import { AsyncStorage } from '@react-native-async-storage/async-storage';
 
 function AlmacenScreen() {
     
@@ -30,14 +31,17 @@ function AlmacenScreen() {
   /* Esta variable almacenara todas las respuestas que obtenemos de DocumentPicker view despues de seleccionar
   un archivo*/ 
   const [fileResponse, setFileResponse] = useState([]);
+  const [placeHolder, setPlaceHolder] = useState (["NOMBRE DE IMAGEN AQUI"]);
+  const [nombre, setNombre] = useState();
 
   const elegirArchivo = async () => {
     const result = await DocumentPicker.getDocumentAsync({
     type: ['audio/*', 'image/*', 'text/plain'],
-    copyToCacheDirectory: false,});
+    copyToCacheDirectory: false});
     /*La opcion de elegir multiples archivos esta falso por Default */
     setFileResponse(result);
     console.log(result);
+    setPlaceHolder(result.name);
     /*
     {Imagen
         "mimeType": "image/jpeg", "name": "Screenshot_20220911-105253_Package installer.jpg", "size": 222554, 
@@ -51,34 +55,79 @@ function AlmacenScreen() {
      "type": "success", "uri": "content://com.android.providers.downloads.documents/document/1036"}
     */ 
   };
+  
+  /*
   const AlmacenarArchivo = async () => {
     const asset = await MediaLibrary.createAssetAsync(fileResponse.uri);
 
-    console.log(asset.creationTime);/*
+    console.log(asset.creationTime);
     const album = await getAlbumAsync('Descargas');
     if(album == null){
       await createAlbumAsync('Descargas', asset, false);
     } else {
       console.log("Entro");
       await addAssetsToAlbumAsync([asset], album, false);
-    }*/
+    }
+  }
+  */
+  //const permiso = useContext(ArchivosContext);
+
+  //FUNCIONES QUE USAN ASYNC STORAGE
+  /*
+  const storeData = async () => {
+    try {
+      console.log(fileResponse);
+      await AsyncStorage.setItem('Database', JSON.stringify(fileResponse))
+    } catch (e) {
+      alert(e);
+    }
   }
 
-  //const permiso = useContext(ArchivosContext);
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('Database')
+      console.log(jsonValue);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch(e) {
+      alert("ERROR");
+    }
+  }
+  
+  //TUTORIAL ASYNC STORAGE
+  const storeName = async () => {
+    try {
+      console.log(nombre);
+      await AsyncStorage.setItem("Database", nombre)
+    } catch (e) {
+      alert(e);
+    }
+  }
+
+  const loadName = async () => {
+    let name = await AsyncStorage.getItem("Database");
+
+    if(name != null)
+    {
+      setNombre(name);
+    }
+  }
+
+  useEffect(() => {
+    loadName()
+  }, [])
+  */
 
   return (
     <ProveedorArchivos>
           <View style={styles.Pantalla}>
             <Separator/>
+                <Text>{placeHolder}</Text>
+                <Separator/>
                 <Button
                 title="Seleccionar archivo"
                 onPress={elegirArchivo}
                 />
-                 <Separator/>
-                <Button
-                title="Almacenar archivo"
-                onPress={AlmacenarArchivo}
-                />
+                <Separator/>
           </View>
     </ProveedorArchivos>
     );
@@ -102,6 +151,17 @@ function AlmacenScreen() {
       uri: {
         paddingBottom: 8,
         paddingHorizontal: 18,
+      },
+      input : {
+        borderWidth: 1,
+        borderColor: "#575DD9",
+        alignSelf: 'stretch',
+        margin: 32,
+        Height: 64,
+        borderRadius: 6,
+        paddingHorizontal: 16,
+        fontSize: 24,
+        fontWeight: "300",
       },
     });
 
